ddb4b1ac80aee6a16173a23700d16da2b968edc4
elon <22001029@qq.com>
2024-04-25 10:48:32 +0800

    增加ignore文件

--- src/main/java/elon/activiti/StartupActivitiApplication.java
+++ src/main/java/elon/activiti/StartupActivitiApplication.java
@@ -3,11 +3,12 @@ package elon.activiti;
 import org.activiti.spring.boot.SecurityAutoConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 /**
  * 服务启动类
  */
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication(exclude = {SecurityAutoConfiguration.class, DataSourceAutoConfiguration.class})
 public class StartupActivitiApplication {
 
     public static void main(String[] args) {
